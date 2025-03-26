9078eb4c0ee89c2bfa98ce882dd409154dbcee9a
ali <ail77erfqagh99@gmail.com>
2025-03-26 15:10:39 +0330

    first commit

---
+++ src/test/java/com/example/actuatior/ActuatiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actuatior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatiorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
